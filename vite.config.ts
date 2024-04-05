dcee68f3819025726c7e3ec3c90b45afedd3592d
DAKuznetsov <DAKuznetsov@synergy.ru>
2024-04-05 11:00:20 +0300

    fix config on ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@public': path.resolve(__dirname, 'public')
     }
-  }
+  },
+  // TODO: похоже не работает с .js https://github.com/vitejs/vite/discussions/14649
+  // server: {
+  //   hmr: { overlay: false }
+  // },
+  define: {
+    'import.meta.env.BASE_URL': JSON.stringify(process.env.VITE_BASE_URL)
+  },
+  base: process.env.NODE_ENV === 'production' ? '/player_with_my_favorite_music/' : '/'
 })
