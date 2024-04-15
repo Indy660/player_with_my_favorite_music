72c0e8b0fc01e7243b84584b6ff381c9c52a3cf9
DAKuznetsov <DAKuznetsov@synergy.ru>
2024-04-15 12:49:00 +0300

    add hash

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
-import { createMemoryHistory, createWebHistory, createRouter } from 'vue-router'
+// import { createMemoryHistory, createWebHistory, createRouter } from 'vue-router'
 
 // const routes = [
 //   { path: '/', component: App },
 //   { path: '/tab=*&track=*', component: App }
 // ]
-const routes = [{ path: '/:pathMatch(.*)*', component: App }]
+// const routes = [{ path: '/:pathMatch(.*)*', component: App }]
 // const routes = [{ path: '/:pathMatch(.*)*', redirect: '/2' }]
 // const routes = [{ path: '/:pathMatch(.*)*', redirect: import.meta.env.BASE_URL }]
 
-const router = createRouter({
-  history: createMemoryHistory(),
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-  // mode: 'history'
-})
+// const router = createRouter({
+//   history: createMemoryHistory(),
+//   // history: createWebHistory(import.meta.env.BASE_URL),
+//   routes
+//   // mode: 'history'
+// })
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -25,5 +25,5 @@ import { far } from '@fortawesome/free-regular-svg-icons'
 library.add(fas, far)
 dom.watch()
 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
-// createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+// createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
