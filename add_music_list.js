54baac75c5f4ff56cef74b95dd343e1e129c0415
Indy <indycar111@gmail.com>
2024-07-11 00:05:52 +0300

    add scripts

--- add_music_list.js
+++ add_music_list.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const { exec } = require('child_process')
 
 // Шаг 1: Чтение JSON-файла
 function readJsonFile(filePath) {
@@ -16,30 +17,42 @@ function readJsonFile(filePath) {
 function addNewSongsToMusicList(jsonFilePath, musicDirPath) {
   // Получаем текущий список песен
   const musicList = readJsonFile(jsonFilePath)
-
   // Обходим директорию с музыкой
   fs.readdir(musicDirPath, { withFileTypes: true }, (err, files) => {
     if (err) throw err
-
     files.forEach((file) => {
       if (path.extname(file.name) === '.mp3') {
-        const songName = path.basename(file.name, '.mp3') // Извлекаем имя песни без расширения
-        const existingSongIndex = musicList.findIndex((song) => song.songName === songName)
+        // (file.name, '.mp3') // Извлекаем имя песни без расширения
+        const existingSongIndex = musicList.findIndex((song) => song.songName === file.name)
 
         // Если песня еще не добавлена, добавляем ее
         if (existingSongIndex === -1) {
-          musicList.push({ songName })
-          console.log(`Добавлена новая песня: ${songName}`)
+          musicList.push({ songName: file.name })
+          console.log(`Добавлена новая песня: ${file.name}`)
         }
       }
     })
-
     // Шаг 5: Записываем изменения обратно в файл
     fs.writeFileSync(jsonFilePath, JSON.stringify(musicList, null, 2), 'utf8')
   })
 }
-
-// Использование функции
 const jsonFilePath = path.join('./src/static_data/music_list.json')
 const musicDirPath = path.join('./public/music')
 addNewSongsToMusicList(jsonFilePath, musicDirPath)
+
+// запуск соседнего скрипта для текста песен
+function runNodeScript(scriptPath) {
+  exec(`node ${scriptPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Ошибка при запуске скрипта ${scriptPath}: `, error)
+      return
+    }
+    console.log(`Результат выполнения скрипта ${scriptPath}:`)
+    console.log(stdout)
+    if (stderr) {
+      console.error(`Ошибка выполнения скрипта ${scriptPath}:`, stderr)
+    }
+  })
+}
+const scriptToRunAfter = './write_lyric_with_timecode.js'
+runNodeScript(scriptToRunAfter)
