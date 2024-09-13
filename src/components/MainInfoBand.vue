5c8f5b1f58e9b25bc7fa7574155838e03e5bd701
Indy <indycar111@gmail.com>
2024-09-14 00:07:01 +0300

    fix image sizes

--- src/components/MainInfoBand.vue
+++ src/components/MainInfoBand.vue
@@ -1,14 +1,6 @@
 <script lang="ts">
-import {
-  defineComponent,
-  ref,
-  onBeforeMount,
-  computed,
-  onUnmounted,
-  onMounted,
-  watchEffect
-} from 'vue'
-import type { ComputedRef, Ref } from 'vue'
+import { defineComponent, ref, onBeforeMount, computed, watchEffect } from 'vue'
+import type { ComputedRef } from 'vue'
 
 export default defineComponent({
   name: 'MainInfoBand',
@@ -30,13 +22,6 @@ export default defineComponent({
   setup(props, { emit }) {
     const imagePaths = ref<Record<string, string>>({})
 
-    const width: Ref<number> = ref(window.innerWidth)
-    const height: Ref<number> = ref(window.innerHeight)
-
-    const updateSize = () => {
-      width.value = window.innerWidth
-      height.value = window.innerHeight
-    }
     onBeforeMount(async () => {
       // :TODO any - хз как исправить
       interface ImagesObject {
@@ -47,12 +32,6 @@ export default defineComponent({
         const imageName: string = path.replace(/^.*\/(.*)\.\w+$/, '$1')
         imagePaths.value[imageName] = (await images[path]()).default
       }
-    })
-    onMounted(() => {
-      window.addEventListener('resize', updateSize)
-    })
-    onUnmounted(() => {
-      window.removeEventListener('resize', updateSize)
     })
 
     const fullSongName: ComputedRef<string> = computed(() => {
@@ -78,18 +57,6 @@ export default defineComponent({
       // TODO: как сделать так, чтобы свг в default_logo заработало? Не работает require в css?
       // const path = imagePaths.value[bandName] || imagePaths.value.default_logo
       // return `url(${path})`
-    })
-
-    interface ImagesSizes {
-      width: number | string
-      height: number | string
-    }
-    const getImageSizes: ComputedRef<ImagesSizes> = computed(() => {
-      function getSize(value: number, coefficient: number = 1): ImagesSizes {
-        return { width: `${coefficient * value}px`, height: `${coefficient * value}px` }
-      }
-      if (width.value > height.value) return getSize(height.value, 0.6)
-      return getSize(width.value, 0.8)
     })
     function setMetadata(): void {
       if ('mediaSession' in navigator && getLogoImage.value) {
@@ -136,7 +103,6 @@ export default defineComponent({
     return {
       getInfoBand,
       getLogoImage,
-      getImageSizes,
       onIconShowTextClick,
       onIconAddFavoriteClick,
       iconHeartClass,
@@ -168,9 +134,12 @@ export default defineComponent({
 <style scoped>
 .main-info {
   .album-image {
-    width: v-bind('getImageSizes.width');
-    height: v-bind('getImageSizes.height');
     background-color: white;
+    aspect-ratio: 1 / 1;
+    max-height: 60vh;
+    max-width: 60vw;
+    width: 100%;
+    height: 100%;
     /*color: green;*/
     /*background-image: v-bind('getLogoImage') ;*/
     /*background-size: contain;*/
