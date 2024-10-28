a50ce2ea07a70dc5af8c3879f164f332c9a24d5a
DAKuznetsov <DAKuznetsov@synergy.ru>
2024-10-28 13:04:08 +0300

    New fixes in loading text songs

--- write_lyric(experimental).js
+++ write_lyric(experimental).js
@@ -3,37 +3,92 @@ const path = require('path')
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-// TODO: разобраться в проблме
-// The Doors - Alabama song.mp3
-// P.O.D. - Youth of the Nation.mp3
-// As I Lay Dying - The Sound Оf Truth
+const apiUrl = 'https://genius.com'
 
-// August-burns-red-the-truth-of-a-liar-lyrics
-// As-i-lay-dying-the-sound-of-truth-lyrics
-axios
-  .get('https://genius.com/As-i-lay-dying-the-sound-of-truth-lyrics', {
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
-  })
-  .then((response) => {
+// TODO: для быстрых тестов
+// axios
+//   .get('https://genius.com/As-i-lay-dying-the-sound-of-truth-lyrics', {
+//     headers: {
+//       'User-Agent':
+//         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+//     }
+//   })
+//   .then((response) => {
+//     const $ = cheerio.load(response.data)
+//
+//     const lyricContainers = $('*[data-lyrics-container="true"]')
+//
+//     // Объединяем содержимое всех найденных элементов
+//     const combinedLyrics = lyricContainers
+//       .map(function () {
+//         return $(this).html()
+//       })
+//       .get()
+//       .join('\n')
+//     console.log(combinedLyrics)
+//     const removeLinks = (text) => text.replace(/<a[^>]*>(.*?)<\/a>/gi, '$1')
+//     const contentWithoutLinks = removeLinks(combinedLyrics)
+//     console.log(1312313, contentWithoutLinks)
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
+async function fetchLyricsAndSave(songTitle, originalName, lyricsData, outputFilePath) {
+  if (lyricsData[originalName]) {
+    console.log(`Данные для ${originalName} уже есть, запрос к API пропущен.`)
+    return
+  }
+  try {
+    const formatBandName = (songTitle) =>
+      songTitle.replace(/\s+/g, '-').replace('---', '-').replace('.', '').toLowerCase()
+    const formatSongTitle = formatBandName(songTitle)
+    console.log(formatSongTitle)
+    const requestUrl = `${apiUrl}/${formatSongTitle}-lyrics`
+    const response = await axios.get(requestUrl, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    })
     const $ = cheerio.load(response.data)
-
+    console.log(response.data)
     const lyricContainers = $('*[data-lyrics-container="true"]')
 
-    // Объединяем содержимое всех найденных элементов
-    const combinedLyrics = lyricContainers
+    const content = lyricContainers
       .map(function () {
         return $(this).html()
       })
       .get()
       .join('\n')
-    console.log(combinedLyrics)
+    console.log(content)
     const removeLinks = (text) => text.replace(/<a[^>]*>(.*?)<\/a>/gi, '$1')
-    const contentWithoutLinks = removeLinks(combinedLyrics)
-    console.log(1312313, contentWithoutLinks)
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+    const contentWithoutLinks = removeLinks(content)
+    lyricsData[originalName] = contentWithoutLinks || ''
+    await writeJsonFile(outputFilePath, lyricsData)
+    console.log(`Текст песни для ${songTitle} сохранен в ${outputFilePath}`)
+  } catch (error) {
+    lyricsData[originalName] = ''
+    await writeJsonFile(outputFilePath, lyricsData)
+    if (error.response.status !== 404) {
+      console.error(`Ошибка при запросе текста песни для ${songTitle}:`, error)
+    } else {
+      console.warn(`Песня ${songTitle} в ${apiUrl} не найдена.`)
+    }
+  }
+}
+
+async function writeJsonFile(filePath, data) {
+  try {
+    await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2))
+    console.log(`Файл ${filePath} успешно обновлен`)
+  } catch (err) {
+    console.error(`Ошибка при записи файла ${filePath}:`, err)
+  }
+}
+
+fetchLyricsAndSave(
+  'August Burns Red - Truth of a Liar',
+  'August Burns Red - Truth of a Liar.mp3',
+  {},
+  'src/static_data/songs_text_new.json'
+)
--- write_lyric.js
+++ write_lyric.js
@@ -13,7 +13,7 @@ const musicListFile = 'music_list.json'
 const apiUrl = 'https://genius.com'
 
 const lyricFolderFileTimecodes = 'songs_text_with_timecodes.json'
-const apiUrlTimecodes = 'https://api.textyl.co/api/lyrics?q='
+const apiUrlTimecodes = 'https://api.textyl.co'
 
 // Чтение и запись JSON файлов с обработкой ошибок
 async function readJsonFile(filePath) {
@@ -37,13 +37,13 @@ async function writeJsonFile(filePath, data) {
 
 // Получение текста песни и сохранение в JSON файл
 async function fetchLyricsAndSave(songTitle, originalName, lyricsData, outputFilePath) {
-  if (lyricsData[originalName]) {
+  if (lyricsData[originalName] || lyricsData[originalName] === '') {
     console.log(`Данные для ${originalName} уже есть, запрос к API пропущен.`)
     return
   }
   try {
     const formatBandName = (songTitle) =>
-      songTitle.replace(/\s+/g, '-').replace('.', '').toLowerCase()
+      songTitle.replace(/\s+/g, '-').replace('---', '-').replace('.', '').toLowerCase()
     const formatSongTitle = formatBandName(songTitle)
     const requestUrl = `${apiUrl}/${formatSongTitle}-lyrics`
     const response = await axios.get(requestUrl, {
@@ -63,7 +63,7 @@ async function fetchLyricsAndSave(songTitle, originalName, lyricsData, outputFil
       })
       .get()
       .join('\n')
-    console.log(content)
+    // console.log(content)
     const removeLinks = (text) => text.replace(/<a[^>]*>(.*?)<\/a>/gi, '$1')
     const contentWithoutLinks = removeLinks(content)
     lyricsData[originalName] = contentWithoutLinks || ''
@@ -74,6 +74,8 @@ async function fetchLyricsAndSave(songTitle, originalName, lyricsData, outputFil
     await writeJsonFile(outputFilePath, lyricsData)
     if (error.response.status !== 404) {
       console.error(`Ошибка при запросе текста песни для ${songTitle}:`, error)
+    } else {
+      console.warn(`Песня ${songTitle} в ${apiUrl} не найдена.`)
     }
   }
 }
@@ -91,16 +93,20 @@ async function fetchLyricsAndSaveTimecodes(
   }
 
   try {
-    const requestUrl = `${apiUrlTimecodes}${encodeURIComponent(songTitle)}`
+    const requestUrl = `${apiUrlTimecodes}/api/lyrics?q=${encodeURIComponent(songTitle)}`
     const response = await axios.get(requestUrl)
 
     lyricsDataTimecodes[originalName] = response.data || []
     await writeJsonFile(outputFilePathTimecodes, lyricsDataTimecodes)
     console.log(`Текст песни для ${songTitle} сохранен в ${outputFilePathTimecodes}`)
   } catch (error) {
-    console.error(`Ошибка при запросе текста песни для ${songTitle}:`, error)
     lyricsDataTimecodes[originalName] = []
     await writeJsonFile(outputFilePathTimecodes, lyricsDataTimecodes)
+    if (error.response.status !== 404) {
+      console.error(`Ошибка при запросе текста песни для ${songTitle}:`, error)
+    } else {
+      console.warn(`Песня ${songTitle} в ${apiUrlTimecodes} не найдена.`)
+    }
   }
 }
 
@@ -109,9 +115,9 @@ async function checkAndUpdateMusicList(fileName, musicListPath, musicList) {
   if (!musicList.some((item) => item.songName === fileName)) {
     musicList.push({ songName: fileName })
     await writeJsonFile(musicListPath, musicList)
-    console.log(`Добавлена новая песня: ${fileName}`)
+    // console.log(`Добавлена новая песня: ${fileName}`)
   } else {
-    console.log(`Песня "${fileName}" уже существует в списке.`)
+    // console.log(`Песня "${fileName}" уже существует в списке.`)
   }
 }
 
